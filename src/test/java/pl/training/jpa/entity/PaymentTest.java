266ef3fff5d328a59c5e04bffab63f33d95545df
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-09 11:57:07 +0100

    base

--- src/test/java/pl/training/jpa/entity/PaymentTest.java
+++ src/test/java/pl/training/jpa/entity/PaymentTest.java
@@ -2,15 +2,12 @@ package pl.training.jpa.entity;
 
 import pl.training.jpa.common.EntityTest;
 
-import java.util.Date;
-
 public class PaymentTest extends EntityTest<Payment> {
 
     @Override
     protected void initializeEntity() {
         entity = Payment.builder()
                 .status(PaymentStatus.STARTED)
-                .timestamp(new Date())
                 .build();
     }
 
