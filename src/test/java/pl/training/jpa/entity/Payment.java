0229470b1b3eea988d4e6fa339dd0f4c06cbbb58
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-09 11:56:22 +0100

    base

--- src/test/java/pl/training/jpa/entity/Payment.java
+++ src/test/java/pl/training/jpa/entity/Payment.java
@@ -1,17 +1,22 @@
 package pl.training.jpa.entity;
 
 import lombok.*;
+import lombok.extern.java.Log;
 import pl.training.jpa.common.Identifiable;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Table(name = "payments", indexes = @Index(name = "payments_time_stamp", columnList = "time_stamp")
     //,uniqueConstraints = @UniqueConstraint(columnNames = "time_stamp")
 )
 @Entity
+@Log
 @Builder
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Payment implements Identifiable<Long> {
@@ -21,7 +26,7 @@ public class Payment implements Identifiable<Long> {
     private Long id;
     @Column(name = "time_stamp", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private Date timestamp;
+    private LocalDateTime timestamp;
     @Enumerated(EnumType.STRING)
     private PaymentStatus status;
 
@@ -40,6 +45,42 @@ public class Payment implements Identifiable<Long> {
     @Override
     public int hashCode() {
         return 31;
+    }
+
+    @PostLoad
+    public void postLoad() {
+        log.info(getClass().getSimpleName() + " PostLoad");
+    }
+
+    @PrePersist
+    public void prePersist() {
+        timestamp = LocalDateTime.now();
+        log.info(getClass().getSimpleName() + " PrePersist");
+    }
+
+    @PostPersist
+    public void postPersist() {
+        log.info(getClass().getSimpleName() + " PostPersist");
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        log.info(getClass().getSimpleName() + " PreUpdate");
+    }
+
+    @PostUpdate
+    public void postUpdate() {
+        log.info(getClass().getSimpleName() + " PostUpdate");
+    }
+
+    @PreRemove
+    public void preRemove() {
+        log.info(getClass().getSimpleName() + " PreRemove");
+    }
+
+    @PostRemove
+    public void postRemove() {
+        log.info(getClass().getSimpleName() + " PostRemove");
     }
 
 }
